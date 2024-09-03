45116a05ec013f7655b1d780b81ecfd6ecd183b5
kei <from0615.ykhm@gmail.com>
2024-09-03 23:32:08 +0900

    add: #9 `addMemo`関数を追加してメモを追加する機能を作成

--- src/hooks/useMemos.ts
+++ src/hooks/useMemos.ts
@@ -3,5 +3,15 @@ import { Memo } from "../types";
 
 export const useMemos = () => {
   const [memos, setMemos] = useState<Memo[]>([]);
-  return { memos };
+
+  const addMemo = (content: string) => {
+    const newMemo: Memo = {
+      id: Date.now().toString(),
+      content,
+      createdAt: new Date().toISOString(),
+    };
+    setMemos([...memos, newMemo]);
+  };
+
+  return { memos, addMemo };
 };
