85e13d5ad890f18a8841bcc2c32fe7f65a1a80d4
kei <from0615.ykhm@gmail.com>
2024-09-04 18:03:58 +0900

    update: インポートのパスを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 // TODO: MemoFormとMemoListコンポーネントを作成する
 import { MemoForm } from "../components/memos/MemoForm";
-import { MemoList } from "../components/MemoList";
+import { MemoList } from "../components/memos/MemoList";
 import { useMemos } from "../hooks/useMemos";
 
 export default function Home() {
