8c39f7826bc4d31ccedc02ed5db303c5f1f948c9
kei <from0615.ykhm@gmail.com>
2024-09-04 15:39:42 +0900

    add: #9 フォームの基本構造と送信処理のTODOを記録

--- src/components/MemoForm.tsx
+++ src/components/MemoForm.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const MemoForm = () => {
-  return <div>MemoForm</div>;
+export const MemoForm: React.FC = () => {
+  return (
+    // TODO: フォームの送信処理を実装する
+    <form action="">
+      <input type="text" placeholder="メモを入力" />
+      <button type="submit">追加</button>
+    </form>
+  );
 };
 
 export default MemoForm;
