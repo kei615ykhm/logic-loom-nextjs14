f24fdc7399bdda5f443e80c55a17623d7d21e482
kei <from0615.ykhm@gmail.com>
2024-09-04 17:56:02 +0900

    remove: ローカルストレージを使ったメモの機能を削除
    - コンポーネントの分離に伴いメモ機能を`useMemo`に移行するため

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,5 @@ import { useMemos } from "../hooks/useMemos";
 export default function Home() {
   const { memos, addMemo, deleteMemo } = useMemos();
 
-  return (
-    <div>
-      <button onClick={() => addMemo("テストメモ")}>メモを追加</button>
-      <ul>
-        {memos.map((memo) => (
-          <li key={memo.id}>
-            {memo.content}
-            <button onClick={() => deleteMemo(memo.id)}>削除</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
