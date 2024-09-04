2cd2cab4dbd1a42ff86335b87659d05faa270e50
kei <from0615.ykhm@gmail.com>
2024-09-04 16:06:22 +0900

    add: `content`が保持する値を自動更新する記述
    - ユーザーが入力した値に応じて自動的に入力フィールドの表示を更新する

--- src/components/MemoForm.tsx
+++ src/components/MemoForm.tsx
@@ -6,7 +6,7 @@ export const MemoForm: React.FC = () => {
   return (
     // TODO: フォームの送信処理を実装する
     <form action="">
-      <input type="text" placeholder="メモを入力" />
+      <input type="text" value={content} placeholder="メモを入力" />
       <button type="submit">追加</button>
     </form>
   );
