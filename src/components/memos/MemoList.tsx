6055d6e6187c2a91effc7c01a75ee60960f26e6e
kei <from0615.ykhm@gmail.com>
2024-09-11 20:59:34 +0900

    docs: `MemoList`コンポーネントに`TSDoc`スタイルのコメントを採用
    - コンポーネントの役割と`props`の説明がマウスオーバーで表示されるようにした

--- src/components/memos/MemoList.tsx
+++ src/components/memos/MemoList.tsx
@@ -12,18 +12,21 @@ interface MemoListProps {
   onDelete: (id: string) => void;
 }
 
-// MemoListコンポーネントを定義
+/**
+ * メモのリストを表示し、各メモの削除機能を提供するコンポーネント
+ * @param props - コンポーネントのプロパティ
+ * @param props.memos - 表示するメモの配列
+ * @param props.onDelete - メモを削除するための関数
+ */
 export const MemoList: React.FC<MemoListProps> = ({ memos, onDelete }) => {
   return (
     <ul className="divide-y divide-gray-200">
-      {/* メモのリストを表示 */}
       {memos.map((memo) => (
         <li
           key={memo.id}
           className="py-4 px-6 flex items-center justify-between hover:bg-gray-50"
         >
           {memo.content}
-          {/* メモ削除ボタンを表示し、クリック時にonDelete関数を呼び出す */}
           <button
             onClick={() => onDelete(memo.id)}
             className="ml-4 px-4 py-2 text-sm text-white bg-red-500 border border-red-500 rounded hover:bg-red-700 hover:border-red-700 focus:outline-none"
