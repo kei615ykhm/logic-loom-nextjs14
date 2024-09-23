2c3dbd2a3a77cc128c410e7fd3de49dc5f944a66
kei <from0615.ykhm@gmail.com>
2024-09-23 22:52:07 +0900

    add: `strict`メソッドを使用して定義してないフィールドが含まれる場合にバリデーションエラーを発生させるようにする

--- src/schemas/memoSchema.ts
+++ src/schemas/memoSchema.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
 /** WARNING: 予期しないデータがスキーマにマッチしてしまう可能性がある */
-export const memoSchema = z.object({
-  id: z.string().uuid({ message: 'IDは有効なUUIDである必要があります' }),
-  content: z.string().min(1, { message: 'メモの内容が空です' }),
-  createdAt: z.string().refine((date) => !isNaN(Date.parse(date)), {
-    message: '無効な日付形式です',
-  }),
-});
+export const memoSchema = z
+  .object({
+    id: z.string().uuid({ message: 'IDは有効なUUIDである必要があります' }),
+    content: z.string().min(1, { message: 'メモの内容が空です' }),
+    createdAt: z.string().refine((date) => !isNaN(Date.parse(date)), {
+      message: '無効な日付形式です',
+    }),
+  })
+  .strict(); // 予期しないプロパティを許可しない
 
 export type Memo = z.infer<typeof memoSchema>;
