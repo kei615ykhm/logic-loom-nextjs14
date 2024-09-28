b948d42dadd6819302a7d642df2459ce096a78ae
kei <from0615.ykhm@gmail.com>
2024-09-28 23:47:06 +0900

    remove: 重複しているバリデーションを削除

--- src/schemas/memoSchema.ts
+++ src/schemas/memoSchema.ts
@@ -3,10 +3,7 @@ import { z } from 'zod';
 export const memoSchema = z
   .object({
     id: z.string().uuid({ message: 'IDは有効なUUIDである必要があります。' }),
-    content: z
-      .string()
-      .min(1, { message: '内容は最低1文字必要です。' })
-      .max(1000, { message: '内容は最大1000文字までです。' }),
+    content: z.string(),
     createdAt: z.string().refine((date) => !isNaN(Date.parse(date)), {
       message: '作成日時は有効な日付である必要があります。',
     }),
