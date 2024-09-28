7dd8d9ec71dd68f3762071789d97040f9d745c35
kei <from0615.ykhm@gmail.com>
2024-09-28 23:45:27 +0900

    add: メモ作成用のスキーマを定義

--- src/schemas/memoSchema.ts
+++ src/schemas/memoSchema.ts
@@ -14,3 +14,12 @@ export const memoSchema = z
   .strict(); // 予期しないプロパティを許可しない
 
 export type Memo = z.infer<typeof memoSchema>;
+
+export const createMemoSchema = z.object({
+  content: z
+    .string()
+    .min(1, { message: '内容は最低1文字必要です。' })
+    .max(1000, { message: '内容は最大1000文字までです。' }),
+});
+
+export type CreateMemo = z.infer<typeof createMemoSchema>;
