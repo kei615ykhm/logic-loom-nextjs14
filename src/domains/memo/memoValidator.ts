2576745ca6dbb2890fd021cc3e7f1dfb2acd78e3
kei <from0615.ykhm@gmail.com>
2024-10-04 23:21:37 +0900

    add: `memoUtils`から分割されたメモの入力バリデーションを作成

---
+++ src/domains/memo/memoValidator.ts
@@ -0,0 +1,11 @@
+import { createMemoSchema, CreateMemoInput } from './memoZodSchema';
+
+/**
+ * 新しいメモ入力をバリデーションする
+ * @param data 検証するデータ
+ * @returns データが有効な場合はCreateMemoInputオブジェクト、そうでない場合はnull
+ */
+export function validateCreateMemoInput(data: unknown): CreateMemoInput | null {
+  const result = createMemoSchema.safeParse(data);
+  return result.success ? result.data : null;
+}
