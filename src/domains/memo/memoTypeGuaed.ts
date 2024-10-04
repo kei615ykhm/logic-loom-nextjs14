466558dbcd4866cb123562f10000b55e09ec60d1
kei <from0615.ykhm@gmail.com>
2024-10-04 23:27:08 +0900

    add: `memoUtils`から分割された`Memo`型かどうかを検証する型ガード関数を作成

---
+++ src/domains/memo/memoTypeGuaed.ts
@@ -0,0 +1,10 @@
+import { memoSchema, Memo } from './memoZodSchema';
+
+/**
+ * データがMemo型かどうかを判定する型ガード関数
+ * @param data 検証するデータ
+ * @returns dataがMemo型の場合true、そうでない場合false
+ */
+export function isMemo(data: unknown): data is Memo {
+  return memoSchema.safeParse(data).success;
+}
