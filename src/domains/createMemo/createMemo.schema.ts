141511a632efd11b0e1942fa1f37e848a2ec1b7f
kei <from0615.ykhm@gmail.com>
2024-10-06 17:31:10 +0900

    update: 型を一致させるため`createdAt`を追加
    - `validatedMemo`が`Memo`型と不一致だったことを修正

--- src/domains/createMemo/createMemo.schema.ts
+++ src/domains/createMemo/createMemo.schema.ts
@@ -2,10 +2,12 @@ import { z } from 'zod';
 
 /** メモ作成用のスキーマ */
 export const createMemoSchema = z.object({
+  id: z.string().uuid({ message: 'IDは有効なUUIDである必要があります。' }),
   content: z
     .string()
     .min(1, { message: '内容は最低1文字必要です。' })
     .max(1000, { message: '内容は1000文字以内でなければなりません。' }),
+  createdAt: z.string(),
 });
 
 export type CreateMemoInput = z.infer<typeof createMemoSchema>;
