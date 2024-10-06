26d84f1821d3a713f46f5661335736d2d857e10a
kei <from0615.ykhm@gmail.com>
2024-10-06 18:41:46 +0900

    add: 状態管理フックの作成

---
+++ src/shared/hooks/useSharedMemoState.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import { z } from 'zod';
+import {
+  Memo,
+  fetchMemoSchema,
+} from '../../domains/fetchMemo/fetchMemo.schema';
+
+export const useSharedMemoState = () => {
+  const [memos, setMemos] = useState<Memo[]>([]);
+
+  useEffect(() => {
+    const storedMemos = localStorage.getItem('memos');
+    if (storedMemos) {
+      try {
+        const parsedMemos: unknown = JSON.parse(storedMemos);
+        const memoArraySchema = z.array(fetchMemoSchema);
+        const validatedMemos: Memo[] = memoArraySchema.parse(parsedMemos);
+        setMemos(validatedMemos);
+      } catch (error) {
+        console.error('保存されたメモの解析に失敗しました:', error);
+      }
+    }
+  }, []);
+
+  const updateLocalStorage = (updatedMemos: Memo[]) => {
+    localStorage.setItem('memos', JSON.stringify(updatedMemos));
+  };
+
+  return { memos, setMemos, updateLocalStorage };
+};
